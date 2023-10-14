a51eae4248d49827bbf187a99a7be09842672c5f
Sbj3333 <skandabharadhwaj100@gmail.com>
2023-10-14 20:22:23 +0530

    firebasetake2

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,16 @@
+
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyDKQKocRCxO-AdPwR1uOMIiSuLDT3fgtl4",
-    authDomain: "weal-8f2aa.firebaseapp.com",
-    projectId: "weal-8f2aa",
-    storageBucket: "weal-8f2aa.appspot.com",
-    messagingSenderId: "275707566787",
-    appId: "1:275707566787:web:0193224746359cd2dde076"
-  };
+  apiKey: "AIzaSyDKQKocRCxO-AdPwR1uOMIiSuLDT3fgtl4",
+  authDomain: "weal-8f2aa.firebaseapp.com",
+  projectId: "weal-8f2aa",
+  storageBucket: "weal-8f2aa.appspot.com",
+  messagingSenderId: "275707566787",
+  appId: "1:275707566787:web:0193224746359cd2dde076"
+};
 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getDatabase(firebaseApp);
 
-export default db
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
